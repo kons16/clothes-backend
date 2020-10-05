e76f0dee9ed5588d6d9bcab3dae44b2d5bc9ca58
kons16 <ks1216r@gmail.com>
2020-10-05 22:20:29 +0900

    Helloを実装

--- web/router.go
+++ web/router.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"github.com/kons16/team7-backend/usecase"
+	"github.com/kons16/team7-backend/web/handler"
 	"net/http"
 )
 
@@ -9,5 +10,9 @@ func NewServer(userUC *usecase.UserUseCase) *http.Server {
 	s := http.Server{
 		Addr: "127.0.0.1:8000",
 	}
+
+	http.HandleFunc("/hello", handler.Hello)
+	http.HandleFunc("/user", handler.Create)
+
 	return &s
 }
