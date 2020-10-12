c4669261bcd1c76f4df9ec90fd5b3e1f22e71a0c
kons16 <ks1216r@gmail.com>
2020-10-12 14:25:57 +0900

    s3にファイルをアップ

--- domain/service/cloth.go
+++ domain/service/cloth.go
@@ -1,6 +1,68 @@
 package service
 
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+// ランダムな文字列の生成
+func MakeRandomStr(digit uint32) (string, error) {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// 乱数を生成
+	b := make([]byte, digit)
+	if _, err := rand.Read(b); err != nil {
+		return "", errors.New("unexpected error...")
+	}
+
+	// letters からランダムに取り出して文字列を生成
+	var result string
+	for _, v := range b {
+		// index が letters の長さに収まるように調整
+		result += string(letters[int(v)%len(letters)])
+	}
+	return result, nil
+}
+
 // base64 のデータを s3 にアップロードして URL を返す
 func UploadS3(imageBase64 string) string {
-	return ""
+	if err := godotenv.Load(); err != nil {
+		return ""
+	}
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Credentials: credentials.NewStaticCredentials(os.Getenv("S3AK"), os.Getenv("S3SK"), ""),
+		Region:      aws.String("ap-northeast-1"),
+	}))
+
+	uploader := s3manager.NewUploader(sess)
+
+	data, _ := base64.StdEncoding.DecodeString(imageBase64)
+	wb := new(bytes.Buffer)
+	wb.Write(data)
+
+	name, _ := MakeRandomStr(10)
+
+	res, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(os.Getenv("S3_KEY")),
+		Key:    aws.String("clothes/" + name + ".jpeg"),
+		Body:   wb,
+	})
+
+	if err != nil {
+		fmt.Println(res)
+	}
+
+	url := os.Getenv("S3_URL") + name + ".jpeg"
+
+	return url
 }
